515c6ecb45c820e747df666e7568f51d73acaa30
Ivan Kulikov <kulikovis91@gmail.com>
2024-03-01 21:26:14 +0300

    drawing box

--- app/randomapp.c
+++ app/randomapp.c
@@ -8,11 +8,29 @@
 
 static bool isInitialized = false;
 
+static void PutPixel(uint8_t x, uint8_t y, bool fill) {
+  UI_DrawPixelBuffer(gFrameBuffer, x, y, fill);
+}
+
+static void DrawBox() {
+    for (uint8_t x = 0; x < 128; ++x) {
+        PutPixel(x, 0, true);
+        PutPixel(x, 55, true);
+    }
+
+    for (uint8_t y = 0; y < 56; ++y) {
+        PutPixel(0, y, true);
+        PutPixel(127, y, true);
+    }    
+}
+
 static void Render() {
     UI_DisplayClear();
 
     UI_PrintString("HELLO", 2, 0, 0, 8);
     UI_PrintString("ZADROSTAN", 2, 0, 3, 8);
+
+    DrawBox();
 
     ST7565_BlitFullScreen();
 }
